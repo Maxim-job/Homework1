7eca2fbf68465a3aed460f35d2624ae601c6cd05
Maxim <max13999@yandex.ru>
2023-01-31 18:39:11 +0300

    4 действие

---
+++ Tusk4/Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.Write("от : ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("до: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
